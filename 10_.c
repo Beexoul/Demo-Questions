9377862c58e90869fac673591686a0b2abc0dfeb
Beexoul <official.beexoul@gmail.com>
2025-03-02 08:19:48 +0545

    uploaded 1 file

--- 10_.c
+++ 10_.c
@@ -1,1 +1,21 @@
-// 
+/* Write a program to generate Fibonacci series upto nth term using recursive function. */
+
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return (fibonacci(n - 1) + fibonacci(n - 2));
+    }
+}
+int main() {
+    int n, i;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    printf("Fibonacci series up to %d terms:\n", n);
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    return 0;
+}
